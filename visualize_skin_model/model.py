8be5803a3f191208c10ae8fcdbd2888faff90b59
Mengjie <ga32lex@mytum.de>
2019-05-29 16:21:25 +0200

    structure model woking for static case

--- visualize_skin_model/model.py
+++ visualize_skin_model/model.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 
 class Node:
     def __init__(self, x, y, z):
@@ -30,14 +31,24 @@ class Floor:
 
 
 class Frame:
-    def __init__(self, floor_geometry, height):
+    def __init__ (self, floors, index):
         """
-        connecting the same points of a floor geometry for all floors
-        """
+        connecting all points from the same geometry point for each floor
+        """ 
+        self.nodes = []
+        self.x_vec, self.y_vec, self.z_vec = [],[],[]
+        for floor in floors:
+            node = floor.nodes[index]
+            self.nodes.append(node)
+            self.x_vec.append(node.x)
+            self.y_vec.append(node.y)
+            self.z_vec.append(node.z)
+
 
 class Structure:
-    def __init__(self, structure_file):
+    def __init__(self, structure_file, density= 5):
         self.floors = []
+        self.frames = []
         with open(structure_file) as json_file:
             data = json.load(json_file)
             self.floor_geometry = data["geometry"]
@@ -48,24 +59,51 @@ class Structure:
             except:
                 self.floor_height = self.structure_height/self.num_of_floors
         
+        self.densify_contour(density)
         self.print_structure_info()
         self.create_floors()
+        self.create_frames()
 
     def print_structure_info(self):    
         msg = "=============================================\n"
         msg += "MODEL INFO \n"
         msg += "HEIGHT:\t" + str(self.structure_height) + "\n"
         msg += "#FLOOR:\t" + str(self.num_of_floors) + "\n"
-        msg += "FLOOR HEIGHT:\t" + str(self.floor_height)
+        msg += "FLOOR HEIGHT:\t" + str(self.floor_height) + "\n"
+        msg += "============================================="
         print(msg)
 
     def create_floors(self):
         current_height = 0.0
         while current_height < self.structure_height:
-            current_height += self.floor_height  
-            print(current_height)
+            current_height += self.floor_height
+            if current_height > self.structure_height:
+                current_height = self.structure_height
             floor = Floor(self.floor_geometry, current_height)
             self.floors.append(floor)
+       
+    def create_frames(self):
+        for i in range(len(self.floor_geometry)):
+            frame = Frame(self.floors, i)
+            self.frames.append(frame)
+
+    def densify_contour(self, parts=5):
+        new_floor_geometry = []
+        for i in range(len(self.floor_geometry)):
+            new_floor_geometry.append(self.floor_geometry[i])
+
+            new_floor_geometry += self._get_equidistant_points(
+                    [self.floor_geometry[i % len(self.floor_geometry)]["x"],
+                     self.floor_geometry[i % len(self.floor_geometry)]["y"]],
+                    [self.floor_geometry[(i+1) % len(self.floor_geometry)]["x"],
+                     self.floor_geometry[(i+1) % len(self.floor_geometry)]["y"]],
+                    parts)
+
+        self.floor_geometry = new_floor_geometry
+        
+    def _get_equidistant_points(self, p1, p2, parts):
+        return [{"x": val1, "y": val2} for val1, val2 in zip(np.linspace(p1[0], p2[0], parts+1),
+                                                             np.linspace(p1[1], p2[1], parts+1))]
 
 if __name__ == "__main__":
     s = Structure("trapezoid.json")
--- visualize_skin_model/visualise_model.py
+++ visualize_skin_model/visualise_model.py
@@ -9,15 +9,46 @@ def visualise_structure(structure):
     fig = plt.figure(figsize=(5, 10))
     ax = fig.add_subplot(111, projection = '3d', aspect=10, azim=-40, elev=10)
 
-    for floor in structure.floors:
-        visualise_floor(floor, ax)
+    visualise_floor(structure.floors, ax)
+    visualise_floor(structure.frames, ax)
     
     plt.tight_layout()
     plt.show()
     
-def visualise_floor(floor, ax):
-    x, y, z  = [], [], []
-    # adding coordinates to one vector    
 
-    z = np.array([floor.z_vec, floor.z_vec])
-    ax.plot_wireframe(floor.x_vec, floor.y_vec, z)
+def visualise_floor(floors, ax):
+    for floor in floors:
+        z = np.array([floor.z_vec, floor.z_vec])
+        ax.plot_wireframe(floor.x_vec, floor.y_vec, z)
+
+
+def visualize_frame(frames, ax):
+    for frame in frames:
+        z = np.array([frame.z_vec, frame.z_vec])
+        ax.plot_wireframe(frame.x_vec, frame.y_vec, z)
+
+
+def animate(self):
+    # Set up formatting for the movie files
+    Writer = animation.writers['ffmpeg']
+    writer = Writer(fps=15, metadata=dict(artist='Me'), bitrate=1800)
+    a = animation.FuncAnimation(fig, self.update, 40, repeat = False)
+    #a.save("structure_displacement.avi")
+    plt.show()
+
+
+def update(self, t):
+    """
+    ploting the deformed structure with respect to the displacement
+    """
+    global fig, ax
+
+    wframe = fig.gca()
+    if wframe!=None:
+        ax.cla()
+
+    self._create_all_floors(t)
+    self.plot_all_floors()
+    self.plot_frame()
+
+
