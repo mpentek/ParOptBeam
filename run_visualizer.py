1225bc221045501ef13d7e9776cb714fd214add2
Mengjie <ga32lex@mytum.de>
2019-10-08 15:53:02 +0200

    removed unnecessary file

--- run_visualizer.py
+++
@@ -1,7 +0,0 @@
-import json
-from os.path import join
-from source.postprocess.visualize_skin_model_utilities import visualize_skin_model
-
-json_file_name = join(
-    *["input", "parameters", "ProjectParameters3DSkinVisualizationUtility.json"])
-visualize_skin_model(json_file_name)
