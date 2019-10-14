dff66b8a178dddc8e8e7799ce66e607694f6a11d
Mengjie <ga32lex@mytum.de>
2019-10-14 09:41:00 +0200

    corrected multiplication

--- source/scheme/bdf2_scheme.py
+++ source/scheme/bdf2_scheme.py
@@ -18,9 +18,9 @@ class BDF2(TimeIntegrationScheme):
         self.dt = dt
 
         # bdf2 scheme coefficients
-        self.bdf0 = 3 * 0.5 / self.dt
-        self.bdf1 = -4 * 0.5 / self.dt
-        self.bdf2 = 1 * 0.5 / self.dt
+        self.bdf0 = 1.5 / self.dt
+        self.bdf1 = -2. / self.dt
+        self.bdf2 = 0.5 / self.dt
 
         # mass, damping and spring stiffness
         self.M = comp_model[0]
@@ -36,17 +36,16 @@ class BDF2(TimeIntegrationScheme):
         self.a0 = initial_conditions[2]
 
         # initial values for time integration
-
         self.an4 = self.a0
         self.vn4 = self.v0
         self.un4 = self.u0
-        self.an3 = - np.dot(self.vn4, self.B) - np.dot(self.un4, self.K) + self.an4
+        self.an3 = - np.dot(self.B, self.vn4) - np.dot(self.K, self.un4) + self.an4
         self.vn3 = self.vn4 + self.an3 * self.dt
         self.un3 = self.un4 + self.vn3 * self.dt
-        self.an2 = - np.dot(self.vn3, self.B) - np.dot(self.un3, self.K) + self.an3
+        self.an2 = - np.dot(self.B, self.vn3) - np.dot(self.K, self.un3) + self.an3
         self.vn2 = self.vn3 + self.an2 * self.dt
         self.un2 = self.un3 + self.vn2 * self.dt
-        self.an1 = - np.dot(self.vn2, self.B) - np.dot(self.un2, self.K) + self.an2
+        self.an1 = - np.dot(self.B, self.vn2) - np.dot(self.K, self.un2) + self.an2
         self.vn1 = self.vn2 + self.an1 * self.dt
         self.un1 = self.un2 + self.vn1 * self.dt
 
@@ -59,31 +58,23 @@ class BDF2(TimeIntegrationScheme):
         print(" ")
 
     def solve_structure(self, f1):
-
-        RHS = - np.dot(self.bdf1 * self.un1, self.B) - np.dot(self.bdf2 * self.un2, self.B)
-        RHS += - 2 * np.dot(self.bdf0 * self.bdf1 * self.un1, self.M)
-        RHS += - 2 * np.dot(self.bdf0 * self.bdf2 * self.un2, self.M)
-        RHS += - np.dot(self.bdf1 * self.bdf1 * self.un2, self.M)
-        RHS += - 2 * np.dot(self.bdf1 * self.bdf2 * self.un3, self.M)
-        RHS += - np.dot(self.bdf2 * self.bdf2 * self.un4, self.M) + f1
+        RHS = - np.dot(self.B, self.bdf1 * self.un1) - np.dot(self.B, self.bdf2 * self.un2)
+        RHS += - 2 * self.bdf0 * self.bdf1 * np.dot(self.M, self.un1)
+        RHS += - 2 * self.bdf0 * self.bdf2 * np.dot(self.M, self.un2)
+        RHS += -     self.bdf1 * self.bdf1 * np.dot(self.M, self.un2)
+        RHS += - 2 * self.bdf1 * self.bdf2 * np.dot(self.M, self.un3)
+        RHS += -     self.bdf2 * self.bdf2 * np.dot(self.M, self.un4) + f1
 
         # calculates self.un0,vn0,an0
-        self.un0 = np.linalg.solve(self.LHS, RHS)
-        self.vn0 = self.bdf0 * self.un0 + self.bdf1 * self.un1 + self.bdf2 * self.un2
-        self.an0 = self.bdf0 * self.vn0 + self.bdf1 * self.vn1 + self.bdf2 * self.vn2
-
-        # update self.u1,v1,a1
-        self.u1 = self.un0
-        self.v1 = self.vn0
-        self.a1 = self.an0
-        # print(self.u1)
+        self.u1 = np.linalg.solve(self.LHS, RHS)
+        self.v1 = self.bdf0 * self.u1 + self.bdf1 * self.un1 + self.bdf2 * self.un2
+        self.a1 = self.bdf0 * self.v1 + self.bdf1 * self.vn1 + self.bdf2 * self.vn2
 
     def update_structure_time_step(self):
         # update self.un3 un2 un1 vn2 vn1
         self.un4 = self.un3
         self.un3 = self.un2
         self.un2 = self.un1
-        self.un1 = self.un0
+        self.un1 = self.u1
         self.vn2 = self.vn1
-        self.vn1 = self.vn0
-
+        self.vn1 = self.v1
