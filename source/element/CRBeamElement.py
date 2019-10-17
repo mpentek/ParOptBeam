9461d6c636d80831d4bd9cee4ff39deebfabb859
Mengjie <ga32lex@mytum.de>
2019-10-17 16:37:53 +0200

    updated part of CR element

--- source/element/CRBeamElement.py
+++ source/element/CRBeamElement.py
@@ -7,6 +7,20 @@ class CRBeamElement(Element):
     def __init__(self, parameters, domain_size):
         super().__init__(parameters, domain_size)
 
+        # element properties
+
+        self.E = self.parameters['e']
+        self.L = self.parameters['lx_i']
+
+        # area
+        self.A = self.parameters['a']
+        # effective area of shear
+        self.Ay = self.parameters['a_sy']
+        self.Az = self.parameters['a_sz']
+        # second moment of inertia
+        self.Iy = self.parameters['iy']
+        self.Iz = self.parameters['iz']
+
         self._print_element_information()
 
     def _print_element_information(self):
@@ -16,7 +30,17 @@ class CRBeamElement(Element):
         """
             element mass matrix derivation from Klaus Bernd Sautter's master thesis
         """
+
     def get_el_stiffness(self, i):
         """
             element stiffness matrix derivation from Klaus Bernd Sautter's master thesis
         """
+
+
+        phi_a_y = 1 / (1 + 12 * E * Iy / (L ** 2 * G * Az))
+        phi_a_z = 1 / (1 + 12 * E * Iz / (L ** 2 * G * Ay))
+
+        # constant stiffness
+        kc_11 = np.array([[E * A / L, 0, 0],
+                          [0, 12 * E * Iz * phi_a_z / L ** 3, 0],
+                          [0, 12 * E * Iy * phi_a_y / L ** 3, 0]])
