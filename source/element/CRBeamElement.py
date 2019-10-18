18c7a33c8ebff4b127b6db32e299f1d9a78fc47c
Mengjie <ga32lex@mytum.de>
2019-10-18 12:07:47 +0200

    finished constant contribultion of the stiffness matrix

--- source/element/CRBeamElement.py
+++ source/element/CRBeamElement.py
@@ -47,12 +47,57 @@ class CRBeamElement(Element):
         """
             element stiffness matrix derivation from Klaus Bernd Sautter's master thesis
         """
+        # shear coefficients
+        phi_a_y = 1 / (1 + 12 * self.E * self.Iy / (self.Li ** 2 * self.G * self.Asz))
+        phi_a_z = 1 / (1 + 12 * self.E * self.Iz / (self.Li ** 2 * self.G * self.Asy))
+
+        # constant contribution to Ke
+        # (eq. 4.158)
+        kc_11 = np.array([[self.E * self.A / self.Li, 0., 0.],
+                          [0., 12 * self.E * self.Iz * phi_a_z / (self.Li ** 3), 0.],
+                          [0., 12 * self.E * self.Iy * phi_a_y / (self.Li ** 3), 0.]])
+
+        kc_33 = kc_11
+        kc_13 = -kc_11
+        kc_31 = -kc_11
+
+        # (eq. 4.159)
+        kc_22 = np.array([[self.G * self.It / self.Li, 0., 0.],
+                          [0., (3 * phi_a_y + 1) * self.E * self.Iy / self.Li, 0.],
+                          [0., 0., (3 * phi_a_z + 1) * self.E * self.Iz / self.Li]])
+
+        kc_44 = kc_22
+
+        # (eq. 4.160.)
+        kc_24 = np.array([[-self.G * self.It / self.Li, 0., 0.],
+                          [0., (3 * phi_a_y - 1) * self.E * self.Iy / self.Li, 0.],
+                          [0., 0., (3 * phi_a_z - 1) * self.E * self.Iz / self.Li]])
+
+        kc_42 = kc_24
+
+        kc_12 = np.array([[0., 0., 0.],
+                          [0., 0., 6 * phi_a_z * self.E * self.Iy / (self.Li ** 2)],
+                          [0., 6 * phi_a_y * self.E * self.Iz / (self.Li ** 2), 0.]])
+
+        kc_14 = kc_12
+        kc_23 = -np.transpose(kc_12)
+        kc_43 = kc_23
+        kc_21 = np.transpose(kc_12)
+        kc_41 = kc_21
+        kc_32 = -kc_12
+        kc_34 = -kc_12
+
+        Ke_const = np.array([[kc_11, kc_12, kc_13, kc_14],
+                             [kc_21, kc_22, kc_23, kc_24],
+                             [kc_31, kc_32, kc_33, kc_34],
+                             [kc_41, kc_42, kc_43, kc_44]])
+
+        print(Ke_const)
+
+        # geometric contribution to Ke
+        # kg_11 = np.array([[0., -Qy / l., 0.],
+        #                   [0., 0., 6 * phi_a_z * self.E * self.Iy / (self.Li ** 2)],
+        #                   [0., 6 * phi_a_y * self.E * self.Iz / (self.Li ** 2), 0.]])
 
 
-        phi_a_y = 1 / (1 + 12 * E * Iy / (L ** 2 * G * Az))
-        phi_a_z = 1 / (1 + 12 * E * Iz / (L ** 2 * G * Ay))
-
-        # constant stiffness
-        kc_11 = np.array([[E * A / L, 0, 0],
-                          [0, 12 * E * Iz * phi_a_z / L ** 3, 0],
-                          [0, 12 * E * Iy * phi_a_y / L ** 3, 0]])
+        return Ke_const
