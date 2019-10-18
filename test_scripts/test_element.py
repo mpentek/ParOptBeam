70d615a1d7ba9457ac8457f6c429367bdfdf8356
Mengjie <ga32lex@mytum.de>
2019-10-18 14:29:20 +0200

    updated test element file

--- test_scripts/test_element.py
+++ test_scripts/test_element.py
@@ -1,69 +1,25 @@
 from source.element.CRBeamElement import CRBeamElement
 
-parameters = {'rho': 160.0,
-              'e': 2.861e8,
-              'nu': 0.1,
-              'zeta': 0.005,
-              'lx': 180,
-              'n_el': 3, 'intervals': []}
-
-system_params = {
-    "defined_on_intervals": [
-        {
-            "interval_bounds": [0.0, 60.0],
-            "length_y": [50.0],
-            "length_z": [35.0],
-            "area": [1750.0],
-            "shear_area_y": [1460.0],
-            "shear_area_z": [1460.0],
-            "moment_of_inertia_y": [178646.0],
-            "moment_of_inertia_z": [364583.0],
-            "torsional_moment_of_inertia": [420175.0],
-            "outrigger_mass": [0.0],
-            "outrigger_stiffness": [0.0]},
-        {
-            "interval_bounds": [60.0, 120.0],
-            "length_y": [45.0],
-            "length_z": [30.0],
-            "area": [1350.0],
-            "shear_area_y": [1125.0],
-            "shear_area_z": [1125.0],
-            "moment_of_inertia_y": [101250.0],
-            "moment_of_inertia_z": [227812.5],
-            "torsional_moment_of_inertia": [238140.0],
-            "outrigger_mass": [0.0],
-            "outrigger_stiffness": [0.0]},
-        {
-            "interval_bounds": [120.0, "End"],
-            "length_y": [40.0],
-            "length_z": [25.0],
-            "area": [1000.0],
-            "shear_area_y": [833.0],
-            "shear_area_z": [833.0],
-            "moment_of_inertia_y": [52083.0],
-            "moment_of_inertia_z": [133333.0],
-            "torsional_moment_of_inertia": [122500.0],
-        }
-    ]
-}
+parameters = {'rho': 10.0, 'e': 100., 'nu': 0.1, 'zeta': 0.05, 'lx_i': 10.}
 
 # # length of one element - assuming an equidistant grid
-parameters['lx_i'] = parameters['lx'] / parameters['n_el']
 
 
 def test():
     element = CRBeamElement(parameters, '3D')
-    element.A = 1350
-    element.Asy = 1460
-    element.Asz = 1460
-    element.Iy = 101250
-    element.Iz = 1125
-    element.It = 122500
+    element.A = [5., 5.]
+    element.Asy = [2., 2.]
+    element.Asz = [1., 1.]
+    element.Iy = [10., 10.]
+    element.Iz = [20., 20.]
+    element.It = [20., 30.]
 
-    element.Py = 12 * element.E * element.Iz / (
-            element.G * element.Asy * element.Li ** 2)
-    element.Pz = 12 * element.E * element.Iy / (
-            element.G * element.Asz * element.Li ** 2)
+    element.Py = [12 * element.E * a / (
+            element.G * b * element.Li ** 2) for a, b in
+                       zip(element.Iz, element.Asy)]
+    element.Pz = [12 * element.E * a / (
+            element.G * b * element.Li ** 2) for a, b in
+                       zip(element.Iy, element.Asz)]
 
     element.get_el_stiffness(0)
 
