1d5ffd200dc6a8924ac2ae511b90ae90a2dcdb23
Mengjie <ga32lex@mytum.de>
2019-10-26 18:05:34 +0200

    added residual calculation for picard

--- source/solving_strategies/strategies/residual_based_picard_solver.py
+++ source/solving_strategies/strategies/residual_based_picard_solver.py
@@ -1,11 +1,13 @@
 # ===============================================================================
 """
-        Derived classes from Solver
+        Derived classes from ResidualBasedSolver
 
 Created on:  15.10.2019
 Last update: 16.10.2019
 """
 # ===============================================================================
+
+import numpy as np
 
 from source.solving_strategies.strategies.rasidual_based_solver import ResidualBasedSolver
 
@@ -16,9 +18,13 @@ class ResidualBasedPicardSolver(ResidualBasedSolver):
         super().__init__(array_time, time_integration_scheme, dt,
                          comp_model, initial_conditions, force, structure_model)
 
-    def calculate_residual(self):
-        # self.ru = f - (M * a_n1 + C * v_n1 + self.K(u_n1) * u_n1)
-        pass
+    def calculate_residual(self, u1):
+        v1 = self.scheme.predict_velocity(u1)
+        a1 = self.scheme.predict_acceleration(v1)
+        f = self.force[:, self.step]
+
+        self.ru = f - (np.dot(self.M, a1) + np.dot(self.B, v1) + np.dot(self.K, u1))
+        print(self.ru)
 
     def calculate_increment(self):
         pass
