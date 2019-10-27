6d8a0397d0515b47f199ed54fc13f11108ce8083
Mengjie <ga32lex@mytum.de>
2019-10-27 02:08:20 +0200

    if added for testing

--- source/solving_strategies/strategies/rasidual_based_solver.py
+++ source/solving_strategies/strategies/rasidual_based_solver.py
@@ -33,24 +33,24 @@ class ResidualBasedSolver(Solver):
 
     def solve_single_step(self):
         self.scheme.solve_single_step(self.force[:, self.step])
-        u1 = self.scheme.get_displacement()
-        ru = self.calculate_residual(u1)
 
         nr_it = 0
+        ru = 1.0
         while abs(np.max(ru)) > TOL and nr_it < MAX_IT:
             u1 = self.scheme.get_displacement()
             ru = self.calculate_residual(u1)
             du = self.scheme.calculate_increment(ru)
             self.scheme.apply_increment_and_update(du)
-            nr_it += 1
             print("Nonlinear iteration: ", str(nr_it))
+            print("ru = {:.2e}".format(abs(np.max(ru))))
+            nr_it += 1
 
     def solve(self):
         # time loop
         for i in range(0, len(self.array_time)):
             self.step = i
             current_time = self.array_time[i]
-            print("time: ", "{0:.2f}".format(current_time))
+            print("time: {0:.2f}".format(current_time))
 
             self.solve_single_step()
 
@@ -58,21 +58,24 @@ class ResidualBasedSolver(Solver):
             self.displacement[:, i] = self.scheme.get_displacement()
             self.velocity[:, i] = self.scheme.get_velocity()
             self.acceleration[:, i] = self.scheme.get_acceleration()
-            self.dynamic_reaction[:, i] = self._compute_reaction(
-                self.displacement[:, i],
-                self.velocity[:, i],
-                self.acceleration[:, i])
 
-            # updating deformation and reaction in the element
-            for e in self.structure_model.elements:
-                e.update_nodal_information(
-                    self.displacement[DOFS_PER_NODE[e.domain_size] * e.index:
-                                      DOFS_PER_NODE[e.domain_size] * e.index +
-                                      DOFS_PER_NODE[e.domain_size] * NODES_PER_LEVEL, i],
-                    self.dynamic_reaction[
-                    DOFS_PER_NODE[e.domain_size] * e.index:
-                    DOFS_PER_NODE[e.domain_size] * e.index +
-                    DOFS_PER_NODE[e.domain_size] * NODES_PER_LEVEL, i])
+            # TODO: remove this once the test in test_schemes works without structure model
+            if self.structure_model is not None:
+                self.dynamic_reaction[:, i] = self._compute_reaction(
+                    self.displacement[:, i],
+                    self.velocity[:, i],
+                    self.acceleration[:, i])
+
+                # updating deformation and reaction in the element
+                for e in self.structure_model.elements:
+                    e.update_nodal_information(
+                        self.displacement[DOFS_PER_NODE[e.domain_size] * e.index:
+                                          DOFS_PER_NODE[e.domain_size] * e.index +
+                                          DOFS_PER_NODE[e.domain_size] * NODES_PER_LEVEL, i],
+                        self.dynamic_reaction[
+                        DOFS_PER_NODE[e.domain_size] * e.index:
+                        DOFS_PER_NODE[e.domain_size] * e.index +
+                        DOFS_PER_NODE[e.domain_size] * NODES_PER_LEVEL, i])
 
             # update results
             self.scheme.update()
