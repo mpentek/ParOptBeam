7f7d62e6a6a53ef309a44f2e12d0b1004c12154b
Mengjie <ga32lex@mytum.de>
2019-10-27 12:16:28 +0100

    added symbolic deriviation file

---
+++ source/solving_strategies/strategies/symbolic_derivation.py
@@ -0,0 +1,88 @@
+"""
+################################################################################################
+###   M * u''(t) + C * u'(t) + K * u(t) = f rewrite 2nd order ODE into system of 1st order ODEs
+###   (I)  v'(t) = ( f - C * v(t) - K * u(t) ) / M = f(t, u, v) = rhs
+###   (II) u'(t) = v(t)
+###   differential equations in the form (d^2)y/(du^2) = (rhs)
+###   rhs: The right-hand side function of the ODE.
+###   Newton's 2nd Law formalism has been kept (rhs = f(t, u, v)/m)
+###   rhs = f - cv - ku
+################################################################################################
+"""
+from sympy import *
+
+init_printing(use_unicode=True)
+
+a_n1, a_n, u_n2, u_n1, u_n, u_nm1, u_nm2, u_nm3, v_n1, v_n, v_nm1, v_nm2, t, dt = symbols(
+    'a1 an u2 u1 un un1 un2 un3 v1 vn vn1 vn2 t dt')
+
+f, C, M, K = symbols('f self.B self.M self.K')
+
+du, ru = symbols('du ru')
+
+
+def euler():
+    # ### euler ###
+    # v_n+1 = v_n + dt f(tn, v_n)
+    print("##### Euler #####")
+
+    v_n = (u_n1 - u_n) / dt
+    v_nm1 = (u_n - u_nm1) / dt
+    a_nm1 = (v_n - v_nm1) / dt
+
+    ru = f - (M * a_nm1 + C * v_nm1 + K * u_nm1)
+    print("ru = ", ru)
+
+    drudu = diff(ru, u_n1)
+    eq_u = ru + drudu * du
+    sol = solve(eq_u, du)
+    du = (sol[0])
+
+    print("du = ", du)
+
+
+def bdf1(self):
+    # ### BDF1 ###
+    # v_n+1 = v_n + dt f(tn+1, v_n+1)
+    print("##### BDF1 #####")
+
+    v_n1 = (u_n1 - u_n) / dt
+    v_n = (u_n - u_nm1) / dt
+    a_n1 = (v_n1 - v_n) / dt
+
+    r_u = f - (M * a_n1 + C * v_n1 + K * u_n1)
+
+    print("ru = ", r_u)
+
+    drudu = diff(r_u, u_n1)
+    eq_u = ru + drudu * du
+    sol = solve(eq_u, du)
+    du = (sol[0])
+
+    print("du = ", du)
+
+
+def bdf2(self):
+    # ### BDF2 ###
+    # v_n+1 = 4/3 v_n - 1/3 v_n-1 + 2/3 dt f(tn+1, v_n+1)
+    print("##### BDF2 #####")
+
+    bdf0, bdf1, bdf2 = symbols('self.bdf0 self.bdf1 self.bdf2')
+
+    v_n1 = bdf0 * u_n1 + bdf1 * u_n + bdf2 * u_nm1
+    v_n = bdf0 * u_n + bdf1 * u_nm1 + bdf2 * u_nm2
+    v_nm1 = bdf0 * u_nm1 + bdf1 * u_nm2 + bdf2 * u_nm3
+
+    a_n1 = bdf0 * v_n1 + bdf1 * v_n + bdf2 * v_nm1
+
+    ru = f - (M * a_n1 + C * v_n1 + K * u_n1)
+
+    print("ru = ", ru)
+
+    drudu = diff(ru, u_n1)
+    eq_u = ru + drudu * du
+    sol = solve(eq_u, du)
+    du = (sol[0])
+
+    print("du = ", du)
+
