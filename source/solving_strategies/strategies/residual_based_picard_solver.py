b25de677da30f50baf94933e5e2b9cc4e7dd672e
Mengjie <ga32lex@mytum.de>
2019-10-28 00:30:12 +0100

    force based residual calculation as in paper

--- source/solving_strategies/strategies/residual_based_picard_solver.py
+++ source/solving_strategies/strategies/residual_based_picard_solver.py
@@ -9,6 +9,7 @@ Last update: 16.10.2019
 import numpy as np
 
 from source.solving_strategies.strategies.rasidual_based_solver import ResidualBasedSolver
+from source.auxiliary.global_definitions import *
 
 
 class ResidualBasedPicardSolver(ResidualBasedSolver):
@@ -17,12 +18,22 @@ class ResidualBasedPicardSolver(ResidualBasedSolver):
         super().__init__(array_time, time_integration_scheme, dt,
                          comp_model, initial_conditions, force, structure_model)
 
-    def calculate_residual(self, u1):
-        v1 = self.scheme.predict_velocity(u1)
-        a1 = self.scheme.predict_acceleration(v1)
-        f = self.force[:, self.step]
+    def calculate_increment(self, ru):
+        du = np.linalg.solve(self.K, ru)
+        return du
 
-        ru = f - (np.dot(self.M, a1) + np.dot(self.B, v1) + np.dot(self.K, u1))
+    def calculate_residual(self, f_ext):
+        q = np.zeros(self.structure_model.n_nodes * DOFS_PER_NODE[self.structure_model.domain_size])
+
+        for e in self.structure_model.elements:
+            start_index = DOFS_PER_NODE[e.domain_size] * e.index
+            end_index = start_index + e.ElementSize
+
+            q[start_index:
+              DOFS_PER_NODE[e.domain_size] * e.index +
+              DOFS_PER_NODE[e.domain_size] * NODES_PER_LEVEL] += e.qe
+
+        q = self.structure_model.apply_bc_by_reduction(q, 'column_vector')
+        ru = f_ext - q
         return ru
-
 
