add more concrete tests -> bug in quaternion calculation!!

test_scripts/test_element.py
@@ -18,6 +18,9 @@ def test_timoshenko_element():
     print(K)
 
 
+TOL = 1e-12
+
+
 def test_crbeam_element():
     material_params = {'rho': 10.0, 'e': 100., 'nu': 0.1, 'zeta': 0.05, 'lx_i': 10., 'is_nonlinear': True}
     element_params = {'a': 5., 'asy': 2., 'asz': 2., 'iy': 10, 'iz': 20, 'it': 20}
@@ -25,20 +28,93 @@ def test_crbeam_element():
     coords = np.array([[1., 0.0, 0.0], [2., 0.0, 0.0]])
     element = CRBeamElement(material_params, element_params, coords, 0, '3D')
 
-    Ke_geo = element._get_element_stiffness_matrix_geometry()
-    ke_mat = element._get_element_stiffness_matrix_material()
+    ke_mat_1 = element._get_element_stiffness_matrix_material()
     K = element.get_element_stiffness_matrix()
 
-    new_displacement = [0.1, 0.0, 0.0, 0.1, 0.0, 0.0,
-                        0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-    element.assign_new_deformation(new_displacement)
+    dp = [0.2, 0.0, 0.0, 0.2, 0.0, 0.0,
+          0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+    element.assign_new_deformation(dp)
+    new_coords = element._get_current_nodal_position()
+    new_coords_sol = [1.2, 0.0, 0.0, 2.0, 0.0, 0.0]
+
+    try:
+        assert (new_coords - new_coords_sol < TOL).all()
+    except AssertionError:
+        print("Mistake in coordinate update")
+
+    l = element._calculate_current_length()
+    l_sol = 0.8
+
+    try:
+        assert l - l_sol < TOL
+    except AssertionError:
+        print("Mistake in current length calculation")
+
+    S = element._calculate_transformation_s()
+
+    S_sol = np.array([
+        [0., 0., 0., -1, 0., 0.],
+        [0., 0., 0., 0., 0., 2 / l],
+        [0., 0., 0., 0., -2 / l, 0.],
+        [-1., 0., 0., 0., 0., 0.],
+        [0., -1., 0., 0., 1., 0.],
+        [0., 0., -1., 0., 0., 1.],
+        [0., 0., 0., 1., 0., 0.],
+        [0., 0., 0., 0., 0., -2 / l],
+        [0., 0., 0., 0., 2 / l, 0.],
+        [1., 0., 0., 0., 0., 0.],
+        [0., 1., 0., 0., 1., 0.],
+        [0., 0., 1., 0., 0., 1.],
+    ])
+
+    try:
+        assert (S - S_sol < TOL).all()
+    except AssertionError:
+        print("Mistake in local transformation matrix S calculation")
+
     element.update_internal_force()
 
-    Ke_geo = element._get_element_stiffness_matrix_geometry()
-    ke_mat = element._get_element_stiffness_matrix_material()
+    r0 = element.Quaternion[0]
+    r1 = element.Quaternion[1]
+    r2 = element.Quaternion[2]
+    r3 = element.Quaternion[3]
+
+    print("Quaternion: " + str(element.Quaternion))
+
+    T_sol = np.array([
+            [r0 ** 2 + r1 ** 2 - r2 ** 2 - r3 ** 2, 2 * (r1 * r2 - r0 * r3), 2 * (r1 * r3 + r0 * r2)],
+            [2 * (r1 * r2 + r0 * r3), r0 ** 2 - r1 ** 2 + r2 ** 2 - r3 ** 2, 2 * (r2 * r3 - r0 * r1)],
+            [2 * (r3 * r1 - r0 * r2), 2 * (r3 * r2 + r0 * r1), r0 ** 2 - r1 ** 2 - r2 ** 2 + r3 ** 2],
+
+    ])
+    T = element.LocalRotationMatrix
+
+
+    try:
+        assert (T - T_sol < TOL).all()
+    except AssertionError:
+        msg = "Mistake in local transformation matrix calculation\n"
+        msg += "T is suppose to be:\n" + str(T_sol)
+        msg += "\nIt is however:\n" + str(T)
+        print(msg)
+
+    dv = element.v
+    dv_sol = np.dot(np.transpose(S), dp)
+    try:
+        assert (dv - dv_sol < TOL).all()
+    except AssertionError:
+        print("Mistake in deformation mode calculation")
+
+    ke_mat_2 = element._get_element_stiffness_matrix_material()
+
     K = element.get_element_stiffness_matrix()
     M = element.get_element_mass_matrix()
-    f_test = np.dot(K, new_displacement)
+
+    try:
+        assert (ke_mat_1 - ke_mat_2 < TOL).all()
+    except AssertionError:
+        print("Ke_const wrong")
+
+    f_test = np.dot(K, dp)
     err = f_test - element.nodal_force_global
-    print(err)
     np.set_printoptions(precision=1)