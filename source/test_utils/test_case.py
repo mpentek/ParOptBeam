455cbd0a288bc2fa2deeb033f45538a5aad3532e
Mate Kelemen <mate.kelemen.12@gmail.com>
2021-05-25 19:02:38 +0200

    remove unnecessary inheritance

--- source/test_utils/test_case.py
+++ source/test_utils/test_case.py
@@ -2,7 +2,4 @@
 import unittest
 
 
-class TestCase(unittest.TestCase):
-    """Class derived from unittest.TestCase that can be extended later if necessary"""
-    def __init__(self, *args, **kwargs):
-        unittest.TestCase.__init__(self, *args, **kwargs)
+TestCase = unittest.TestCase
